a95969a44394b256b9d78ffeceaf3412b45e5fe4
zaky <mohamed_zaky@epam.com>
2020-03-27 16:14:06 +0300

    add only_db

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 #echo "connection sting : mosql [-c $collections] [--sql $postgres] [--mongo $mongo]"
 env
 echo "mosql -c collections.yml --sql $POSTGRESQL --mongo $MONGODB"
-mosql -c collections.yml --sql $POSTGRESQL --mongo $MONGODB
+mosql -c collections.yml --sql $POSTGRESQL --mongo $MONGODB --only-db $DBNAME
