e9f15890c6604f79473fc22e10e39227dc797fb9
zaky <mohamed_zaky@epam.com>
2020-03-27 13:14:06 +0300

    test 13

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #echo "connection sting : mosql [-c $collections] [--sql $postgres] [--mongo $mongo]"
 env
-echo "mosql [-c collections.yml] [--sql $POSTGRESQL] [--mongo $MONGODB]"
-mosql [-c collections.yml] [--sql $POSTGRESQL] [--mongo $MONGODB]
+echo "mosql -c collections.yml --sql $POSTGRESQL --mongo $MONGODB"
+mosql -c collections.yml --sql $POSTGRESQL --mongo $MONGODB
